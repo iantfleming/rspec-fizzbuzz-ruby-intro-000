81da8a2b97461d6a862499c166a8a097b744d003
Ian Fleming <iantfleming1992@gmail.com>
2020-10-12 13:26:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "fizz"
+  elsif int % 5 ==
+    return "buzz"
+  elsif int % 3 && in % 5 ==
+    return "fizzbuzz"
+  else
+    return nil
 
 end
