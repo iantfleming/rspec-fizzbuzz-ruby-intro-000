ff5aa48562197db50b53c1fa9b2353f6d3e71fe8
Ian Fleming <iantfleming1992@gmail.com>
2020-10-12 13:34:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,4 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    puts "fizz"
-  elsif int % 5 ==
-    puts "buzz"
-  elsif int % 3 && in % 5 ==
-    puts "fizzbuzz"
-  else
-    puts nil
-  end  
 end
